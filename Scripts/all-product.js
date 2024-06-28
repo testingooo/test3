d28da22dd752edb9833f66cebfedf47992b67f81
testingooo <warisganiu201@gmail.com>
2024-06-28 02:20:11 +0100

    Add files via upload

--- Scripts/all-product.js
+++ Scripts/all-product.js
@@ -2,4 +2,4 @@ import { accessoriesData } from "./Data/accessories-data.js";
 import { airgunData } from "./Data/airgun-data.js";
 import { babyData } from "./Data/baby-bata.js";
 import { beautyData } from "./Data/beauty-data.js";
-export let allProducts = [...accessoriesData, ...babyData, ...beautyData, ...airgunData]
+export let allProducts = [...airgunData,...accessoriesData, ...babyData, ...beautyData]
